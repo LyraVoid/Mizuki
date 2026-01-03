055dbcd913c1790a2f1dc36b5daa6d1f9d200694
h-lyf <mail@666411.xyz>
2026-01-03 22:12:13 +0800

    modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "御剑乘风の小破站",
 	subtitle: "",
-	siteURL: "https://mizuki.666411.xyz/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://blog.666411.xyz/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-11-19", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
