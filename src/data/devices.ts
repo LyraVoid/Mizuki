f1b35ba65add324bfc4aadf76c9a7622aaa0e1ce
youssef <157864954+0xjoyo@users.noreply.github.com>
2026-08-16 06:29:21 +0300

    Keep only current devices

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -19,7 +19,7 @@ export const devicesData: DeviceCategory = {
 			image: "",
 			specs: "Intel Core i5-7440HQ / 16 GB RAM / GeForce 940MX 2 GB",
 			description:
-				"Current laptop used for programming, Linux work, virtualization, and CS/AI learning.",
+				"Main laptop for programming, Linux work, virtualization, and CS/AI learning.",
 			link: "https://www.dell.com/support/home/en-us/product-support/product/latitude-15-5580-laptop/overview",
 		},
 	],
@@ -27,23 +27,9 @@ export const devicesData: DeviceCategory = {
 		{
 			name: "iPhone X",
 			image: "",
-			specs: "Daily iPhone",
-			description: "Current iPhone used as part of the personal device setup.",
+			specs: "iPhone X",
+			description: "Current iPhone in the personal device setup.",
 			link: "https://support.apple.com/kb/SP770",
-		},
-		{
-			name: "iPad mini 1",
-			image: "",
-			specs: "1st generation iPad mini",
-			description: "Older Apple tablet kept as part of the device collection.",
-			link: "https://support.apple.com/kb/SP661",
-		},
-		{
-			name: "iPhone 5c",
-			image: "",
-			specs: "iPhone 5c",
-			description: "Older iPhone kept as part of the device collection and experimentation setup.",
-			link: "https://support.apple.com/kb/SP684",
 		},
 	],
 	Android: [
@@ -51,7 +37,7 @@ export const devicesData: DeviceCategory = {
 			name: "Redmi 9",
 			image: "",
 			specs: "Xiaomi Redmi 9",
-			description: "Android device used for system experiments, Linux/mobile OS exploration, and testing.",
+			description: "Android device used for mobile OS experiments, Linux exploration, and testing.",
 			link: "https://www.mi.com/global/redmi-9/",
 		},
 	],
